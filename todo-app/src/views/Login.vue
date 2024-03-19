be54cede7d92d142e4366ea9f0b348b47df09b26
Touseef Hussain <touseef.hussain@tylextech.com>
2024-03-19 16:01:55 +0500

    update front-end

--- todo-app/src/views/Login.vue
+++ todo-app/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fill-height justify="center" align="center">
     <v-row justify="center" align="center">
-      <v-col class="text-h4"> TODO APP </v-col>
+      <v-col class="text-h4"> NOTE APP </v-col>
     </v-row>
   </v-container>
 
