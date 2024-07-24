5c255ebc9f57dc008d12ecb746c4912046661793
mouazmalik <mouazmalikmouazmalik@gmail.com>
2024-07-24 21:29:23 +0500

    vital commit

--- Todo/installs.sh
+++ Todo/installs.sh
@@ -10,6 +10,4 @@ apt-get install -y python3-pip
 # Install Python packages from requirements file
 pip3 install -r /var/www/html/Todo/requirements.txt
 
-# Install setuptools
-pip3 install setuptools
 
