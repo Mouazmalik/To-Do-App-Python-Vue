e50f82ed587110da2f3f8df44e27c60d6480adc5
mouazmalik <mouazmalikmouazmalik@gmail.com>
2024-07-25 01:01:29 +0500

    port

--- validate.sh
+++ validate.sh
@@ -3,4 +3,4 @@
 curl -f http://18-234-31-124:8000 || exit 1
 
 # Validate that the frontend is running (if applicable)
-curl -f http://18-234-31-124 || exit 1
+curl -f http://18-234-31-124:5173 || exit 1
