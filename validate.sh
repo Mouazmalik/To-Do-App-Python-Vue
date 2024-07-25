2eccd50b1e8b116b101e3de8b2349fd652b62bf2
mouazmalik <mouazmalikmouazmalik@gmail.com>
2024-07-25 18:49:32 +0500

    ip cnage

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Validate that the backend is running
-curl -f http://18-234-31-124:8000 || exit 1
+curl -f http://54.221.102.69:8000 || exit 1
 
 # Validate that the frontend is running (if applicable)
-curl -f http://18-234-31-124:5173 || exit 1
+curl -f http://54.221.102.69:5173 || exit 1
